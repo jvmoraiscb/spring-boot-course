3715edd6f1af57cac17e1e3b9b2823ec7efdaf5f
João Victor Morais <jv.moraiscb@gmail.com>
2022-08-06 21:31:10 -0300

    Learning Spring Boot

---
+++ spring-boot/README.md
@@ -0,0 +1,2 @@
+<a href="https://www.youtube.com/watch?v=9SGDpanrc8U">Spring Boot Tutorial | Full Course [2022] [NEW]</a>
+by Amigoscode
---
+++ spring-boot/src/main/java/com/jvmoraiscb/springboot/student/StudentService.java
@@ -0,0 +1,17 @@
+package com.jvmoraiscb.springboot.student;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+public class StudentService {
+    public List<Student> getStudents() {
+		return List.of(
+			new Student(1L,
+						"João",
+						"jv.moraiscb@gmail.com",
+						LocalDate.of(2003, Month.JUNE, 6),
+						19)
+		);
+	}
+}
