c88220580f1f06a465195ccd047056f782ed1fc7
João Victor Morais <jv.moraiscb@gmail.com>
2022-09-23 01:15:15 -0300

    :white_check_mark: API can POST and DELETE informations from the database

--- src/main/java/com/jvmoraiscb/springboot/student/StudentRepository.java
+++ src/main/java/com/jvmoraiscb/springboot/student/StudentRepository.java
@@ -1,9 +1,14 @@
 package com.jvmoraiscb.springboot.student;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long>{
     
+    // SELECT * FROM student WHERE email = ?
+    // @Query("SELECT s FROM Student s WHERE s.email = ?1")
+    Optional<Student> findStudentByEmail(String email);
 }
